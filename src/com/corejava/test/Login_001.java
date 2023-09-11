819904b539ef6df7521de13a883d2a449f910ea0
tutorpatil <tutorpatil@gmail.com>
2023-09-11 09:04:04 +0530

    Added 2 more Test cases

--- src/com/corejava/test/Login_001.java
+++ src/com/corejava/test/Login_001.java
@@ -4,6 +4,8 @@ public class Login_001 {
 
 	public static void main(String[] args) {
 			System.out.println(" Automating the login_001 Test case...");
+			System.out.println(" Automating the login_002 Test case...");
+			System.out.println(" Automating the login_003 Test case...");
 
 	}
 
